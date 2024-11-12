7c64bbb4ccb3b26f8b89dfa5b8d549c18baa5944
Laiacastle <laia.castillo.7e6@itb.cat>
2024-11-12 13:34:00 +0100

    Exercici 14. Closes #6

--- Activitats2/Fourteen.cs
+++ Activitats2/Fourteen.cs
@@ -1,27 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Activitats2
+/*namespace Activitats2
 {
-    internal class Fourteen
+    
+    public class Fourteen
     {
-        public static int Max(num)
+        public static int Max(int num, int num2)
         {
-            int max;
+            
+            return num > num2 ?  num :  num2;
 
-            return max;
         }
-        public static int Min()
+        public static int Min(int num, int num2)
         {
-            int min;
-            return min;
+            return num == Max(num, num2) ? num2 : num;
         }
         public static void Main()
         {
-            const string MsgOne = "";
+
+            const string MsgOne = "Write two numbers please";
+            const string MsgError = "Wrong Format";
+            const string MsgEnd = "The max is : {0} and de min is: {1}";
+            Console.WriteLine(MsgOne);
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(MsgEnd, Max(num, num2), Min(num, num2));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(MsgError);
+            }
         }
     }
 }
+*/
