38cbf31355f41bb33a0e684c59ae0de02e190bf6
Laiacastle <laia.castillo.7e6@itb.cat>
2024-11-25 12:00:57 +0100

    Diagrama de fluxe CLoses #18

--- acfour_laia_castillo/Program.cs
+++ acfour_laia_castillo/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace AcFor
+{
+    public class AcFor
+    {
+        public static void Main()
+        {
+            const string MsgOne = "Write the numbers you want to almacenar";
+            const string Error = "WrongFormat";
+            const string MsgEnd = "the numbers are: ";
+
+            Console.WriteLine(MsgOne);
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine())
+            }
+        }
+    }
+}
